2fb31b35f8312b7f47ac1d027a3eef41d287bdfd
David Halewood <davidnhalewood@hotmail.com>
2017-10-10 17:04:39 +0100

    adding battle and removing redundant files

--- app.rb
+++ app.rb
@@ -1,27 +1,9 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/' do
-  'Hello!'
-end
+class Battle < Sinatra::Base
+  get '/' do
+    'Hello Battle!'
+  end
 
-get '/secret' do
-  'Shh!'
-end
-
-get '/public' do
-  'Yo!'
-end
-
-get '/random-cat' do
-  @name = %w(Jon David Slim Jebvther).sample
-  erb(:index)
-end
-
-get '/cat-form' do
-  erb(:cat_form)
-end
-
-post '/named-cat' do
-  @name = params[:name]
-  erb(:index)
+  run! if app_file == $0
 end
