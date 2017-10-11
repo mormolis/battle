b0d2c1fbd97aab6e037b4298929a5d8afee7703a
mormolis <george.lamprakis@gmail.com>
2017-10-11 17:49:54 +0100

    Introduces session variables

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
   get "/" do
     erb(:index)
   end
 
   post "/names" do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
-    erb(:names)
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    
+    redirect "/play"
   end
 
+  get "/play" do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
+    erb(:play)
+  end
 
   run! if app_file == $0
 end
