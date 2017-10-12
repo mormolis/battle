088ce81657a70c3bc7e45aeeed428b9b4f9266b7
Canace Wong <canacewongtoday@hotmail.com>
2017-10-12 14:59:50 +0100

    redirect users to play route

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,7 +7,7 @@ feature "So that a player can win they can attack" do
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
-    expect(page).not_to have_content "Alex has 60HP"
-    expect(page).to have_content "David attacked Alex! Alex HP reduced by 10 to 50!" 
+    click_link 'OK'
+    expect(page).to have_content "Alex has 50HP"
   end
 end
