4548860c7ac1afe3a39f788d9e7dc24174fbfe55
David <davidhaletothewood@Davids-MacBook-Pro.local>
2017-10-12 14:35:28 +0100

    adding players to game initialize method

--- app.rb
+++ app.rb
@@ -1,26 +1,33 @@
 require 'sinatra/base'
 require_relative './lib/player'
+require_relative './lib/game'
 
 class Battle < Sinatra::Base
+
   get '/' do
     erb(:index)
   end
+
   enable :sessions
 
   post '/names' do
-    $player1 = Player.new(params[:name1])
-    $player2 = Player.new(params[:name2])
+    # $player1 = Player.new(params[:name1])
+    # $player2 = Player.new(params[:name2])
+    @player_1 , @player_2 = Player.new(params[:name1]), Player.new(params[:name2])
+    $game = Game.new(@player_1, @player_2)
+    @game = $game
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name, @player_2_name = $player1.name, $player2.name
+    @game = $game
     erb(:play)
   end
 
   post '/attack' do
-    @player_1, @player_2 = $player1, $player2
-    Game.new.attack(@player_2)
+    @game = $game
+    @player_2 = @game.player_2
+    @game.attack(@player_2)
     erb(:attack)
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,16 @@
 require 'game'
 describe Game do
-  let(:player_2) {double :player_2}
+  let(:player_1) { double :player_1 }
+  let(:player_2) { double :player_2 }
+  let(:game) { described_class.new(player_1, player_2) }
+
+  it 'initializes with two player instances' do
+    expect(game.player_1).to eq player_1
+  end
   describe '#attack' do
     it 'initiates an attack on the player given' do
       expect(player_2).to receive(:receive_damage)
-      subject.attack(player_2)
+      game.attack(player_2)
     end
   end
 
